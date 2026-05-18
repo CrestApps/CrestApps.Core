f6ee5e50952110d4b14f05d196d7006a2bd62f7c
Mike Alhayek <mike@crestapps.com>
2026-05-18 15:29:32 -0700

    simplify

--- src/Abstractions/CrestApps.Core.Abstractions/ExtensibleEntity.cs
+++ src/Abstractions/CrestApps.Core.Abstractions/ExtensibleEntity.cs
@@ -1,5 +1,5 @@
-using System.Text.Json;
-using System.Text.Json.Nodes;
+using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace CrestApps.Core;
@@ -7,94 +7,12 @@ namespace CrestApps.Core;
 /// <summary>
 /// Base class for entities that support dynamic extensible properties.
 /// </summary>
-public abstract class ExtensibleEntity : IJsonOnDeserialized, IJsonOnSerialized, IJsonOnSerializing
+public abstract class ExtensibleEntity
 {
-    private IDictionary<string, object> _serializedProperties;
-
     /// <summary>
-    /// Gets or sets the dictionary of additional properties that are not explicitly
-    /// declared on the entity.
+    /// Gets or sets the dictionary of additional properties stored under the
+    /// <c>Properties</c> JSON object.
     /// </summary>
-    [JsonExtensionData]
-    public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
-
-    void IJsonOnDeserialized.OnDeserialized()
-    {
-       var properties = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-
-       if (Properties.TryGetValue(nameof(Properties), out var propertyValue))
-       {
-           MergeNestedProperties(properties, propertyValue);
-       }
-
-       Properties = properties;
-    }
-
-    void IJsonOnSerializing.OnSerializing()
-    {
-        _serializedProperties = Properties ?? new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-        Properties = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase)
-        {
-            [nameof(Properties)] = JsonSerializer.SerializeToNode(_serializedProperties, ExtensibleEntityExtensions.JsonSerializerOptions)?.DeepClone() as JsonObject ?? [],
-        };
-    }
-
-    void IJsonOnSerialized.OnSerialized()
-    {
-        Properties = _serializedProperties ?? new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
-        _serializedProperties = null;
-    }
-
-    private static void MergeNestedProperties(
-        Dictionary<string, object> properties,
-        object propertyValue)
-    {
-        if (propertyValue is null)
-        {
-            return;
-        }
-
-        if (propertyValue is JsonObject propertyObject)
-        {
-            foreach (var property in propertyObject)
-            {
-                properties[property.Key] = property.Value?.DeepClone();
-            }
-
-            return;
-        }
-
-        if (propertyValue is JsonElement jsonElement && jsonElement.ValueKind == JsonValueKind.Object)
-        {
-            foreach (var property in jsonElement.EnumerateObject())
-            {
-                properties[property.Name] = property.Value.Clone();
-            }
-
-            return;
-        }
-
-        if (propertyValue is IDictionary<string, object> propertyDictionary)
-        {
-            foreach (var property in propertyDictionary)
-            {
-                properties[property.Key] = CloneExtensionValue(property.Value);
-            }
-
-            return;
-        }
-
-        throw new JsonException($"'{nameof(Properties)}' must be a JSON object.");
-    }
-
-    private static object CloneExtensionValue(object value)
-    {
-        return value switch
-        {
-            null => null,
-            JsonNode jsonNode => jsonNode.DeepClone(),
-            JsonElement jsonElement => jsonElement.Clone(),
-            _ => value,
-        };
-    }
+    [JsonConverter(typeof(ExtensibleEntityPropertiesJsonConverter))]
+    public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 }
