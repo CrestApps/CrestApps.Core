0c0138c5ab5baa0dfef5f59a8755e0584ebb600d
Mike Alhayek <mike@crestapps.com>
2026-09-21 06:16:07 -0700

    Release 1.2.1 (#211)

--- src/CrestApps.Core.Docs/sidebars.js
+++ src/CrestApps.Core.Docs/sidebars.js
@@ -97,6 +97,7 @@ const sidebars = {
             items: [
                 'changelog/index',
                 'changelog/1.3.0',
+                'changelog/1.2.1',
                 'changelog/1.2.0',
                 'changelog/1.1.0',
                 'changelog/1.0.0',
